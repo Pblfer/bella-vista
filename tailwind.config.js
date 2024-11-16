a9f89add2f658297b038c6355a189d7f0653f58f
conversion-team <pblfertec@gmail.com>
2024-11-15 18:20:19 -0600

    fix 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
       fontFamily: {
         sans: ['"mont"', ...defaultTheme.fontFamily.sans],
         badScript: ["badScript"],
+        mont: ["montregular"],
       },
     },
   },
